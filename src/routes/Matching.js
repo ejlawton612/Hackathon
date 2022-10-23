f611414766da59e7589acdefb87a293bbad4cf75
ejlawton612 <106289134+ejlawton612@users.noreply.github.com>
2022-10-22 23:16:07 -0400

    Update Matching.js

--- src/routes/Matching.js
+++ src/routes/Matching.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import TrainingSection from '../components/MatchingC'
+import {useLocation} from "react-router-dom";
+
 import ApartmentImage from '../components/ApartmentImage'
 
 const Matching = () => {
+    const location = useLocation();
+    const [preferences, setPreferences] = useState([[]]);
+    const [listing, setListing] = useState([[]]);
+    const [importance, setImportance] = useState([[]]);
+    const list = () => {
+        if (location.state != null & location.state.listing != null) {
+            setListing(location.state.listing);
+        }
+    }
+    const imp = () => {
+        if (location.state != null && location.state.importance != null) {
+            setImportance(location.state.listing);
+        }
+    }
+    const pref = () => {
+        if (location.state != null && location.state.preferences != null) {
+            setImportance(location.state.preferences);
+        }
+    }
+    useEffect(list, []);
+    useEffect(imp, []);
+    useEffect(pref, []);
     return (
         <div>
-            <Navbar />
+            <Navbar state = {location.state}/>
             <ApartmentImage heading='MATCHING.' text='Find your best match.' />
-            <TrainingSection />
             <Footer />
         </div>
     )
