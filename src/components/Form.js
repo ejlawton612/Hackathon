ff7b63d45bb68f39a3bc86c65d730fd824b8e428
ejlawton612 <106289134+ejlawton612@users.noreply.github.com>
2022-10-22 20:43:55 -0400

    Update Form.js
    Added functionality to form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,100 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import './FormStyles.css'
-
+import {Link, useLocation} from "react-router-dom";
+import Select from "react-dropdown-select";
 const Listing = () => {
+    const location = useLocation();
+    console.log(location);
+    useEffect(() => {console.log(location)},[]);
+    const [name, setName] = useState("");
+    const [cost, setCost] = useState("");
+    const [listing, setListing] = useState([[]]);
+    const [sqf, setSqf] = useState("");
+    const [rooms, setRooms] = useState("");
+    const [bath, setBath] = useState("");
+    const [loc, setLoc] = useState("");
+    const [safety, setSafety] = useState("");
+    const list = () => {
+        if (location.state !== null && location.state.listing !== null) {
+            setListing(location.state.listing);
+        }
+    }
+    useEffect(list, []);
+    const add = (event) => {
+        event.preventDefault();
+        console.log(listing === "");
+        console.log("listing array", listing);
+        if (name !== "" && cost !== "" && sqf !== "" && rooms !== "" && bath !== "" && loc !== "" && safety !== "") {
+            let temp = [name, cost, sqf, rooms, bath, loc, safety];
+            if (listing === [[]]) {
+                setListing([temp]);
+            } else {
+                setListing(listing.concat([temp]));
+            }
+            setName("")
+            setCost("")
+            setSafety("")
+            setLoc("")
+            setBath("")
+            setRooms("")
+            setSqf("")
+        }
+    }
+    const changeName = (event) => {
+        setName(event.target.value);
+    }
+    const changeCost = (event) => {
+        if (!isNaN(event.target.value)){
+            setCost(event.target.value);
+        }
+    }
+    const changeSqf = (event) => {
+        if (!isNaN(event.target.value)) {
+            setSqf(event.target.value);
+        }
+    }
+    const changeSafety = (event) => {
+        setSafety(event.target.value);
+    }
+    const changeBath = (event) => {
+        if (!isNaN(event.target.value)) {
+            setBath(event.target.value);
+        }
+    }
+    const changeRooms = (event) => {
+        if (!isNaN(event.target.value)) {
+            setRooms(event.target.value);
+        }
+    }
+    const changeLoc = (event) => {
+        if (!isNaN(event.target.value)) {
+            setLoc(event.target.value);
+        }
+    }
     return (
         <div className='listing'>
-            <form>
+            <form onSubmit = {add}>
                 <label>Listing Name</label>
-                <input type='text'></input>
+                <input value = {name} onChange = {changeName}/>
                 <label>Number of Bathrooms</label>
-                <input type='#'></input>
+                <input value = {bath} onChange = {changeBath}/>
                 <label>Number of Bedrooms</label>
-                <input type='text'></input>
+                <input value = {rooms} onChange= {changeRooms}/>
                 <label>Safety Level</label>
-                <input type='text'></input>
-                <label>Details</label>
-                <textarea rows='6' placeholder='Type a short message here' />
-                <button className='btn'>Submit</button>
+                <select value = {safety} onChange = {changeSafety}>
+                    <option value = "">Select Safety Level</option>
+                    <option value = "1">Very Safe</option>
+                    <option value = "2">Safe</option>
+                    <option value = "3">Somewhat Safe</option>
+                    <option value = "4">Unsafe</option>
+                </select>
+                <label>Cost</label>
+                <input value = {cost} onChange = {changeCost}/>
+                <label>Square Feet</label>
+                <input value = {sqf} onChange = {changeSqf}/>
+                <label>Location</label>
+                <input value = {loc} onChange = {changeLoc}/>
+                <button type = "submit" className='btn'>Add</button>
             </form>
         </div>
     )
