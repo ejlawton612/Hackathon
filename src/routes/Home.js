4d8307f4fffab2f5b875b17f19bd539e40534c00
ejlawton612 <106289134+ejlawton612@users.noreply.github.com>
2022-10-22 23:17:18 -0400

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Video from '../components/Video'
+import {useLocation} from "react-router-dom";
 
 const Home = () => {
+const location = useLocation();
     return (
         <div>
-            <Navbar />
+            <Navbar state = {location.state}/>
             <Video />
             <Footer />
         </div>
